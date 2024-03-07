61934307367b0f4c121f50559793272ad3ad076b
Hoangminh249 <97183505+Hoangminh249@users.noreply.github.com>
2024-03-07 20:11:18 +0700

    baseline css & create branch dev

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,5 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import Button from "@mui/material/Button";
-import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
